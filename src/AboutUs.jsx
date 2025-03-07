c8d73d8eeaa3e28049b2bfb3966cc3fcdef4d99e
Mareena Castelino <109801636+Mareena-castelino@users.noreply.github.com>
2025-03-07 09:46:39 +0530

    Add files via upload

---
+++ src/AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+// import FacesBehindIndustry from './components/FacesBehindIndustry';
+
+import CoreValueSection from '../components/CoreValueSection';
+
+import SuccessHistoryPage from '../components/SuccessHistoryPage';
+const AboutUs = () => {
+  return (
+    <div className="font-sans text-gray-900 space-y-16 px-6 md:px-0 ">
+      <SuccessHistoryPage/>
+      <CoreValueSection/>
+
+    
+
+    </div>
+  );
+};
+
+export default AboutUs;
