4d1b0f6352a05139e637496d696c1ce37da66840
Mareena Castelino <109801636+Mareena-castelino@users.noreply.github.com>
2025-03-07 10:13:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-
-import ScrollingLogos from './components/ScrollingLogos';
 import ExpertiseSection from './components/ExpertiseSection';
-import CoreValueSection from './components/CoreValueSection';
-/*import HistorySection from './components/HistorySection';*/
-import SuccessHistory from './components/SuccessHistory';
+import ScrollingLogos from './components/ScrollingLogos';
+
+import AboutUs from "./Pages/AboutUs"
 const App = () => {
   return (
     <div className="font-sans text-gray-900">
-      <SuccessHistory/>
-      <CoreValueSection/>
+      <AboutUs/>
       <ScrollingLogos/>
+     
       <ExpertiseSection/>
     
 
